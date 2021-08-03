47cd02f16591bc528a908e0f116cd31efe4c2488
lllilllilllilili <hik3562@hufs.ac.kr>
2021-08-03 10:50:29 +0900

    feat: 🎸 index.js store추가

--- mytour-front-app/src/index.js
+++ mytour-front-app/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(rootReducer, composeWithDevTools);
 
 ReactDOM.render(
   <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-  ,document.getElementById("root")
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
