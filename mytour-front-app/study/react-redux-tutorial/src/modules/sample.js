93f3d267636e2289f4f10da8228ef7e6afae8acd
lllilllilllilili <hik3562@hufs.ac.kr>
2021-08-16 18:09:33 +0900

    fix: 🐛 액션생성함수에 USER_INFO_PASSWORD2 정의, 비밀번호 확인 4자리 타이핑 수정

--- mytour-front-app/study/react-redux-tutorial/src/modules/sample.js
+++ mytour-front-app/study/react-redux-tutorial/src/modules/sample.js
@@ -51,61 +51,61 @@ export const getPost = createRequestThunk(GET_POST, api.getPost);
 export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false,
-  },
+  //   loading: {
+  //     GET_POST: false,
+  //     GET_USERS: false,
+  //   },
   post: null,
   users: null,
 };
 
 const sample = handleActions(
   {
-    [GET_POST]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true,
-      },
-    }),
+    // [GET_POST]: (state) => ({
+    //   ...state,
+    //   loading: {
+    //     ...state.loading,
+    //     GET_POST: true,
+    //   },
+    // }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
+      //   loading: {
+      //     ...state.loading,
+      //     GET_POST: false,
+      //   },
       post: action.payload,
     }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
-    }),
-    [GET_USERS]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: true,
-      },
-      users: true,
-    }),
+    // [GET_POST_FAILURE]: (state, action) => ({
+    //   ...state,
+    //   loading: {
+    //     ...state.loading,
+    //     GET_POST: false,
+    //   },
+    // }),
+    // [GET_USERS]: (state) => ({
+    //   ...state,
+    //   loading: {
+    //     ...state.loading,
+    //     GET_USERS: true,
+    //   },
+    //   users: true,
+    // }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false,
-      },
+      //   loading: {
+      //     ...state.loading,
+      //     GET_USERS: false,
+      //   },
       users: action.payload,
     }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false,
-      },
-    }),
+    // [GET_USERS_FAILURE]: (state, action) => ({
+    //   ...state,
+    //   loading: {
+    //     ...state.loading,
+    //     GET_USERS: false,
+    //   },
+    // }),
   },
   initialState
 );
